43a22dfadb50dffeb64ee6d265d49ee958041443
Mike Brocchi <mbrocchi@gmail.com>
2019-10-03 09:05:09 -0400

    Added tests - results of the twitch stream

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,12 +1,71 @@
-import { TestBed } from '@angular/core/testing';
-
 import { DataService } from './data.service';
+import { Person } from './sw.models';
+import { cold } from 'jasmine-marbles';
+import { TestScheduler } from 'rxjs/testing';
+import { throttleTime } from 'rxjs/operators';
 
-describe('DataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('DataService', () => {
+  let mockPeople;
+  beforeEach(() => {
+    mockPeople = [
+      { name: 'Juke Piewalker' },
+      { name: 'Sobe Han' }
+    ] as Person[];
+  });
+  describe('getPeople', () => {
+    let mockHttp;
+    let dataService: DataService;
+    beforeEach(() => {
+      mockHttp = jasmine.createSpyObj('HttpClient', ['get']);
+      dataService = new DataService(mockHttp);
+    });
+    it('should get people', () => {
+      const httpCall = cold('a|', { a: { results: mockPeople } });
+      mockHttp.get.and.returnValue(httpCall);
+      const actual = dataService.getPeople();
+      const expected = cold('a|', { a: mockPeople });
+      expect(actual).toBeObservable(expected);
+    });
+    it('should return an empty array when the http call fails', () => {
+      const httpGetResult = cold('#');
+      mockHttp.get.and.returnValue(httpGetResult);
+      const actual = dataService.getPeople();
+      //                 bag of marbles
+      const expected = cold('(a|)', { a: [] });
+      expect(actual).toBeObservable(expected);
+    });
+    fit('should fail if the http call takes too long', () => {
+      // const httpCall = cold('2000ms', { a: { results: mockPeople } });
+      // mockHttp.get.and.returnValue(httpCall);
+      // const actual = dataService.getPeople();
+      // const expected = cold('-#');
+      // expect(actual).toBeObservable(expected);
+      const testScheduler = new TestScheduler((a, e) => {
+        // asserting the two objects are equal
+        // e.g. using chai.
+        expect(a).toEqual(e);
+      });
+      testScheduler.run(helpers => {
 
-  it('should be created', () => {
-    const service: DataService = TestBed.get(DataService);
-    expect(service).toBeTruthy();
+        const { expectObservable } = helpers;
+        const httpCall = helpers.cold('2000ms');
+        mockHttp.get.and.returnValue(httpCall);
+        const actual = dataService.getPeople();
+        // const expected = cold('1000ms (a|)', { a: [] });
+        // expect(actual).toBeObservable(expected);
+        expectObservable(actual
+          .pipe(throttleTime(1, testScheduler)))
+          .toBe('(a|)', { a: [] });
+      });
+    });
+    it('should fail if the http call takes too long 2', () => {
+      const httpCall = cold('2000ms |');
+      mockHttp.get.and.returnValue(httpCall);
+      dataService.getPeople().subscribe(people => {
+        expect(people).toEqual([]);
+      });
+      // const expected = cold('1000ms -(a|)', { a: [] });
+      // expect(actual).toBeObservable(expected);
+    });
   });
 });
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Person, Film, Starship, Planet } from './sw.models';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
+import { map, catchError, timeout } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,31 +13,42 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
+  // getPeople(selector: () => {}): Observable<Person[]> {
   getPeople(): Observable<Person[]> {
     const url = `${this.baseUrl}/people`;
-    return this.http.get<any>(url).pipe(
-      map(result => result.results)
-    );
+    return this.getDataSet<Person>(url);
+  }
+
+  getPeopleNames(): Observable<string[]> {
+    return null;
   }
 
   getPlanets(): Observable<Planet[]> {
     const url = `${this.baseUrl}/planets`;
-    return this.http.get<any>(url).pipe(
-      map(result => result.results)
-    );
+    return this.getDataSet<Planet>(url);
   }
 
   getStarships(): Observable<Starship[]> {
     const url = `${this.baseUrl}/starships`;
-    return this.http.get<any>(url).pipe(
-      map(result => result.results)
-    );
+    return this.getDataSet<Starship>(url);
   }
 
   getFilms(): Observable<Film[]> {
     const url = `${this.baseUrl}/films`;
+    return this.getDataSet<Film>(url);
+  }
+
+  getData<T>(url: string): Observable<T> {
+    return this.http.get<any>(url);
+  }
+
+  private getDataSet<T>(url: string): Observable<T[]> {
     return this.http.get<any>(url).pipe(
-      map(result => result.results)
+      timeout(1000),
+      map(result => result.results),
+      catchError(err => {
+        return of([]);
+      })
     );
   }
 }
